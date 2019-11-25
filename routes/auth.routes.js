1f56fa5343b1c7439dec75b675592736983fb970
Sandra Tejedor delso <sandratejedordelso@MacBook-Pro-de-Sandra.local>
2019-11-25 10:37:49 +0100

    nada que añadir

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const passport = require('passport');
 const router = express.Router();
 const User = require("../models/User.model");
+const {
+  ensureLoggedIn,
+  ensureLoggedOut
+} = require('connect-ensure-login');
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
